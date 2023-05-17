f2313f817b5803a202d446426b11d42803c179f9
Diego Torres Facundo <n-diegotorres@hotmail.com>
2023-05-17 03:19:30 -0600

    No sé usar stash

--- app/src/main/java/org/bedu/proyecto01equipo16/Nutrition.kt
+++ app/src/main/java/org/bedu/proyecto01equipo16/Nutrition.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+private lateinit var adapter: CustomAdapter
+private lateinit var recyclerView: RecyclerView
+private lateinit var titlesArraylist: ArrayList<workouts>
+
+lateinit var imageId: Array<Int>
+lateinit var titles: Array<String>
+lateinit var details: Array<String>
 
 /**
  * A simple [Fragment] subclass.
@@ -55,5 +65,45 @@ class Nutrition : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataInitialize()
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView=view.findViewById(R.id.recyclerView)
+        recyclerView.layoutManager=layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter= CustomAdapter(titlesArraylist)
+        recyclerView.adapter = adapter
+    }
+
+    private fun dataInitialize(){
+       titlesArraylist = arrayListOf<workouts>()
+        val imageId = intArrayOf(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,)
+
+        titles = arrayOf(
+            getString(R.string.titles_a),
+            getString(R.string.titles_b),
+            getString(R.string.titles_c),
+            getString(R.string.titles_d)
+
+        )
+        details = arrayOf(
+            getString(R.string.details_a),
+            getString(R.string.details_b),
+            getString(R.string.details_c),
+            getString(R.string.details_d)
+
+        )
+
+        for(i in imageId.indices){
+            val details = workouts(titles[i], details[i])
+            titlesArraylist.add(details)
+        }
     }
 }
