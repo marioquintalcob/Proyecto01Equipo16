685ba10087d2ad8a9b92e7d838c18b913109ce4d
marioquintalcob <119343518+marioquintalcob@users.noreply.github.com>
2023-05-17 10:20:22 -0600

    Feature/navbar (#4)
    * Prueba del login con un listado de usuarios y contraseñas
    
    * Primera Version de Navigation bar
    
    * Primera Version de Navigation bar
    
    * actualice el listado de correos para el login y le di funcionalidad al boton de recuperar contraseña
    
    * si el correo existe el boton de recuperar contraseña envia a otra pantalla. agregue elementos a la pantalla de registro.
    
    * subo dos archivos que no se subieron en el anterior.
    
    ---------
    
    Co-authored-by: Daniel <eguzmanh04@gmail.com>

--- app/src/main/java/org/bedu/proyecto01equipo16/RecopassActivity.kt
+++ app/src/main/java/org/bedu/proyecto01equipo16/RecopassActivity.kt
@@ -1,11 +1,49 @@
 package org.bedu.proyecto01equipo16
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class RecopassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recopass)
+
+        // funcionalidad del boton de recuperar contraseña
+        boton = findViewById(R.id.btnPass)
+        email = findViewById(R.id.txtEmail2)
+
+        boton.setOnClickListener {
+            val emails = listOf("mario.e.quintal.cob@hotmail.com", "n-diegotorres@hotmail.com", "yaelramirezmendez@gmail.com", "eguzmanh04@gmail.com" )
+
+            val emailIngresado = email.text.toString()
+
+            var emailExistente = false
+
+            for (e in emails) {
+                if (emailIngresado == e) {
+                    emailExistente = true
+                    break
+                }
+            }
+
+            if (emailExistente) {
+                val bundle = Bundle()
+                val intent = Intent(this, SendpassActivity::class.java ).apply{
+                    putExtras(bundle)
+                }
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(this, "Correo no registrado, favor de verificar", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
+    private lateinit var boton: Button
+    private lateinit var email: TextView
 }
