9af814929e6fefa06727f5e840e72f87be5f4299
Diego Torres Facundo <n-diegotorres@hotmail.com>
2023-05-18 08:47:37 -0600

    No more imports unused

--- app/src/main/java/org/bedu/proyecto01equipo16/CustomAdapter.kt
+++ app/src/main/java/org/bedu/proyecto01equipo16/CustomAdapter.kt
@@ -4,7 +4,6 @@ package org.bedu.proyecto01equipo16
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
