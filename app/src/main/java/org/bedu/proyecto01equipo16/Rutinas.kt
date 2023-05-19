c593a78a186d92b51d4a0948719baa635995e448
Diego Torres Facundo <n-diegotorres@hotmail.com>
2023-05-19 13:57:05 -0600

    Listo

--- app/src/main/java/org/bedu/proyecto01equipo16/Rutinas.kt
+++ app/src/main/java/org/bedu/proyecto01equipo16/Rutinas.kt
@@ -8,8 +8,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -21,11 +19,6 @@ lateinit var imagenejercicio: Array<Int>
 lateinit var nombreejercicio: Array<String>
 lateinit var details: Array<String>
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Nutrition.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Rutinas : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -48,15 +41,6 @@ class Rutinas : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Nutrition.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Rutinas().apply {
