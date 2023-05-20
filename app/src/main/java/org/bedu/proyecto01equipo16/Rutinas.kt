98392d4a4383471690af68a8cdc09547dd1936d9
Diego Torres Facundo <n-diegotorres@hotmail.com>
2023-05-19 19:01:45 -0600

    Se limpiaron código autogenerado y variables no utilizadas

--- app/src/main/java/org/bedu/proyecto01equipo16/Rutinas.kt
+++ app/src/main/java/org/bedu/proyecto01equipo16/Rutinas.kt
@@ -1,15 +1,14 @@
 package org.bedu.proyecto01equipo16
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 private lateinit var adapter: CustomAdapter
 private lateinit var recyclerView: RecyclerView
@@ -24,14 +23,6 @@ class Rutinas : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,16 +31,6 @@ class Rutinas : Fragment() {
         return inflater.inflate(R.layout.fragment_nutrition, container, false)
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Rutinas().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
