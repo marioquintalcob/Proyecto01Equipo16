5fae8bf6b516d9f0bfb48e3ed5da3f62a67c1fb1
marioquintalcob <119343518+marioquintalcob@users.noreply.github.com>
2023-05-21 10:27:54 -0600

    Icono y splash (#10)
    * Prueba del login con un listado de usuarios y contraseñas
    
    * Primera Version de Navigation bar
    
    * Primera Version de Navigation bar
    
    * actualice el listado de correos para el login y le di funcionalidad al boton de recuperar contraseña
    
    * si el correo existe el boton de recuperar contraseña envia a otra pantalla. agregue elementos a la pantalla de registro.
    
    * subo dos archivos que no se subieron en el anterior.
    
    * Cambie el icono e hice un splash siguiendo un tutorial, se pueden modificar.
    
    * Cambié gif por un .png, más se agrego un progressbar para el SplashScreen
    
    ---------
    
    Co-authored-by: Daniel <eguzmanh04@gmail.com>
    Co-authored-by: Diego Torres Facundo <n-diegotorres@hotmail.com>

---
+++ app/src/main/java/org/bedu/proyecto01equipo16/Splash.kt
@@ -0,0 +1,35 @@
+package org.bedu.proyecto01equipo16
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class Splash : AppCompatActivity() {
+
+    val DURACION: Long = 3000;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        //full screen
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val logo = findViewById<ImageView>(R.id.muscle_logo)
+
+        Glide.with(this).load(R.drawable.muscle_logo_512).into(logo)
+
+        cambiarActivity()
+    }
+
+    private fun cambiarActivity(){
+        Handler().postDelayed(Runnable{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, DURACION)
+    }
+}
