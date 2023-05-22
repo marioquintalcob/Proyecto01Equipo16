0efde242db1897e6083050ac799a34204be2b3da
Yael Ramírez <yaelramirezmendez@gmail.com>
2023-05-22 07:06:39 -0600

    feature(Register): Added text changed listener (#20)

--- app/src/main/java/org/bedu/proyecto01equipo16/login/RegisterActivity.kt
+++ app/src/main/java/org/bedu/proyecto01equipo16/login/RegisterActivity.kt
@@ -1,12 +1,31 @@
 package org.bedu.proyecto01equipo16.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.bedu.proyecto01equipo16.R
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import org.bedu.proyecto01equipo16.databinding.ActivityInputBinding
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var binding: ActivityInputBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.input_data)
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.inputLayout1.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                binding.textUserName.text = binding.inputLayout1.text
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+        })
     }
 }
